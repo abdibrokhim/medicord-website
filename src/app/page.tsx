e1c2b92869572b4186b665f54a2380e39fbe08d5
abdibrokhim <abdibrokhim@gmail.com>
2024-07-18 16:06:07 +0500

    update comedai page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-import Image from "next/image"; 
+'use client'
+
+import { useState } from 'react';
+import Image from "next/image";
 import './globals.css';
 
 import Footer from './components/footer';
@@ -7,32 +10,65 @@ import { Analytics } from '@vercel/analytics/react';
 
 import blackLogo from '../app/assets/imgs/CoMed_Inc_black.png';
 
-// font-family: Gruppo;
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
-export default function Home() 
+import { firebaseConfig } from './firebase.config';
+import { Subscriber } from './types/subscriber';
 
-{ 
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default function Home() {
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleGetUpdates = async () => {
+    if (!email) return;
+
+    setLoading(true);
+
+    try {
+      const docRef = await addDoc(collection(db, 'subscribers'), {
+        email,
+        createdAt: Date.now(),
+      });
+
+      console.log("Document written with ID: ", docRef.id);
+
+      setEmail('');
+      setShowMessage(true);
+      setTimeout(() => {
+        setShowMessage(false);
+      }, 3000);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-
     <div className="flex flex-col w-full min-h-screen">
       <Analytics />
-      <HeaderPrimary/>
+      <HeaderPrimary />
       <main className="flex-1 flex flex-col gap-16 px-4 md:px-0 py-24 m-auto w-full md:w-4/5 2xl:w-3/5">
         <div className="flex flex-col md:flex-row justify-center md:items-left items-center gap-16">
           <div className="text-left flex md:flex-row flex-col gap-32 py-32 h-full px-16 md:px-0">
-            <div className="md:w-1/2 w-full"> 
+            <div className="md:w-1/2 w-full">
               <span className="flex flex-col gap-4 text-[#1e1e1e] text-[16px] mt-4">
                 <Image
                   src={blackLogo}
                   width={150}
                   alt="Medicord logo"
-                  className="max-w-[300px] img-shadow shrink-0 grow-0" 
+                  className="max-w-[300px] img-shadow shrink-0 grow-0"
                   title="Medicord logo"
                 />
                 <p className="mt-2">CoMed Inc. is a software development company.</p>
                 <p>We believe innovative software can revolutionize the healthcare industry by enhancing the accuracy of diagnoses and integrating seamlessly into hospital and clinic workflows.</p>
                 <p>We contribute to better patient care by fostering a supportive community among healthcare professionals and patients worldwide, leading to improved medical outcomes and a more interconnected healthcare system.</p>
-              </span> 
+              </span>
             </div>
             <div className="flex flex-col justify-center md:w-1/2 w-full">
               <div>
@@ -42,20 +78,35 @@ export default function Home()
                 <input
                   type="text"
                   placeholder="Enter your email"
-                  className="border border-gray-300 rounded-md p-2 w-full max-w-sm outline-noneborder border-[#1e1e1e] rounded-md p-2 w-full max-w-sm shadow-[0_4px_6px_-1px_rgba(30,30,30,0.1),0_2px_4px_-1px_rgba(30,30,30,0.06)] focus:outline-none focus:ring-2 focus:ring-[#1e1e1e]"
-                  />
-                <a className="cta-top flex gap-2 items-center justify-center rounded-md hover:opacity-80 active:opacity-60 active:scale-95 transition-all text-white text-[16px] px-6 h-[40px] shrink-0 grow-0 btn hover:opacity-80 !bg-[#1e1e1e] !text-white !bg-none !text-[18px] px-6 !h-[44px]" href="https://abdibrokhim.vercel.app">
-                  <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" className="-ml-2 shrink-0 iconify iconify--fluent" width="20" height="20" viewBox="0 0 28 28">
-                    <path fill="currentColor" d="M10.06 18.701a1.628 1.628 0 0 0 2.43-.676l.77-2.34a3.823 3.823 0 0 1 2.416-2.418l2.238-.727a1.61 1.61 0 0 0 .786-.595a1.62 1.62 0 0 0-.849-2.489l-2.215-.72a3.82 3.82 0 0 1-2.42-2.414l-.727-2.237a1.622 1.622 0 0 0-.594-.785a1.655 1.655 0 0 0-1.879 0a1.627 1.627 0 0 0-.6.8L8.68 6.365a3.817 3.817 0 0 1-2.359 2.37l-2.24.726a1.626 1.626 0 0 0 .02 3.073l2.216.72a3.856 3.856 0 0 1 1.816 1.286c.266.343.471.728.606 1.14l.728 2.234c.112.318.32.593.595.787m9.744 6.08a1.2 1.2 0 0 0 .696.22a1.2 1.2 0 0 0 .692-.217a1.21 1.21 0 0 0 .446-.6l.372-1.143a1.609 1.609 0 0 1 1.017-1.02l1.166-.378A1.213 1.213 0 0 0 25 20.505a1.2 1.2 0 0 0-.844-1.146l-1.144-.37a1.609 1.609 0 0 1-1.02-1.018l-.38-1.163a1.2 1.2 0 0 0-2.274.016l-.374 1.146a1.61 1.61 0 0 1-.993 1.017l-1.166.378a1.213 1.213 0 0 0-.807 1.139a1.2 1.2 0 0 0 .823 1.138l1.144.372a1.606 1.606 0 0 1 1.02 1.023l.379 1.163a1.2 1.2 0 0 0 .44.582"></path>
-                  </svg>
-                  <p className="whitespace-nowrap">Get updates</p>
-                </a>
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="border border-gray-300 rounded-md p-2 w-full max-w-sm outline-none border border-[#1e1e1e] rounded-md p-2 w-full max-w-sm shadow-[0_4px_6px_-1px_rgba(30,30,30,0.1),0_2px_4px_-1px_rgba(30,30,30,0.06)] focus:outline-none focus:ring-2 focus:ring-[#1e1e1e]"
+                />
+                <button
+                  onClick={handleGetUpdates}
+                  disabled={loading}
+                  className="cta-top flex gap-2 items-center justify-center rounded-md hover:opacity-80 active:opacity-60 active:scale-95 transition-all text-white text-[16px] px-6 h-[40px] shrink-0 grow-0 btn hover:opacity-80 !bg-[#1e1e1e] !text-white !bg-none !text-[18px] px-6 !h-[44px]"
+                >
+                  {loading ? 'Loading...' : (
+                    <>
+                      <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" className="-ml-2 shrink-0 iconify iconify--fluent" width="20" height="20" viewBox="0 0 28 28">
+                        <path fill="currentColor" d="M10.06 18.701a1.628 1.628 0 0 0 2.43-.676l.77-2.34a3.823 3.823 0 0 1 2.416-2.418l2.238-.727a1.61 1.61 0 0 0 .786-.595a1.62 1.62 0 0 0-.849-2.489l-2.215-.72a3.82 3.82 0 0 1-2.42-2.414l-.727-2.237a1.622 1.622 0 0 0-.594-.785a1.655 1.655 0 0 0-1.879 0a1.627 1.627 0 0 0-.6.8L8.68 6.365a3.817 3.817 0 0 1-2.359 2.37l-2.24.726a1.626 1.626 0 0 0 .02 3.073l2.216.72a3.856 3.856 0 0 1 1.816 1.286c.266.343.471.728.606 1.14l.728 2.234c.112.318.32.593.595.787m9.744 6.08a1.2 1.2 0 0 0 .696.22a1.2 1.2 0 0 0 .692-.217a1.21 1.21 0 0 0 .446-.6l.372-1.143a1.609 1.609 0 0 1 1.017-1.02l1.166-.378A1.213 1.213 0 0 0 25 20.505a1.2 1.2 0 0 0-.844-1.146l-1.144-.37a1.609 1.609 0 0 1-1.02-1.018l-.38-1.163a1.2 1.2 0 0 0-2.274.016l-.374 1.146a1.61 1.61 0 0 1-.993 1.017l-1.166.378a1.213 1.213 0 0 0-.807 1.139a1.2 1.2 0 0 0 .823 1.138l1.144.372a1.606 1.606 0 0 1 1.02 1.023l.379 1.163a1.2 1.2 0 0 0 .44.582"></path>
+                      </svg>
+                      <p className="whitespace-nowrap">Get updates</p>
+                    </>
+                  )}
+                </button>
               </div>
+              {showMessage && (
+                <div className="mt-4 text-green-500">
+                  Thank you for subscribing!
+                </div>
+              )}
             </div>
           </div>
         </div>
       </main>
-    <Footer/>
-    </div> 
-  ); 
+      <Footer />
+    </div>
+  );
 }
--- src/app/products/comedai/page.tsx
+++ src/app/products/comedai/page.tsx
@@ -1,27 +1,103 @@
-import Image from "next/image"; 
+'use client'
+
+import Image from "next/image";
 import '../../globals.css';
 
 import Footer from '../../components/footer';
 import HeaderPrimary from '../../components/header.primary';
 import { Analytics } from '@vercel/analytics/react';
+import { TypeAnimation } from 'react-type-animation';
 
+import addObservation from '../../assets/ss/add-observation.png';
+import manageObservation from '../../assets/ss/manage-observations.png';
+import generateReport from '../../assets/ss/generate-report.png';
+import generatedReport from '../../assets/ss/generated-report.png';
+import profileOrganization from '../../assets/ss/profile-organization.png';
+import saveConclusion from '../../assets/ss/save-conclusion.png';
 
-export default function CoMedAI() 
+export default function CoMedAI() {
 
-{ 
   return (
-
     <div className="flex flex-col w-full min-h-screen">
       <Analytics />
-      <HeaderPrimary/>
-      <main className="flex-1 flex flex-col gap-16 px-4 md:px-0 py-24 m-auto w-full md:w-4/5 2xl:w-3/5">
-        <div className="flex flex-col md:flex-row justify-center items-center gap-16">
-          <div className="text-left flex flex-row gap-32 py-32 h-full">
-            {`hi, buddy;)`}
+      <HeaderPrimary />
+      <main className="flex-1 flex flex-col gap-16 px-4 md:px-0 m-auto w-full md:w-4/5 2xl:w-3/5">
+        <div className="flex flex-col justify-center items-center">
+          <div className="text-center flex flex-row py-52 text-[200px] font-bold text-black">
+            CoMedAI
+          </div>
+          <div className="text-center flex flex-col gap-8 py-8 items-center w-full max-w-5xl">
+            <a href="https://rebrand.ly/comedai" target="_blank">
+              <p className="text-[18px] bg-[#1e1e1e] py-4 px-6 text-white rounded-md">
+                CoMedAI 1.0 is OUT with a new UI and features! 🎉 <span className="font-bold ms-2"> Try it now 
+                  <svg width="28px" height="28px" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg" className="inline-block ml-2">
+                    <path d="M6 12H18M18 12L13 7M18 12L13 17" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                  </svg>
+                </span>
+              </p>
+            </a>
+            <p className="text-[48px] mt-4 px-6 text-black">
+              The easiest way to generate, manage and report Medical Data.
+            </p>
+            <p className="text-[24px] px-6 text-black">
+              Enhances diagnostic accuracy and streamlines workflows for radiologists.
+            </p>
+            <a href="https://rebrand.ly/comedai" target="_blank" 
+              className="cta-top flex gap-2 mt-4 items-center justify-center rounded-md hover:opacity-80 active:opacity-60 active:scale-95 transition-all text-white text-[18px] px-6 h-[44px] shrink-0 grow-0 btn bg-[#1e1e1e]">
+              <p className="whitespace-nowrap">Get Started 
+                <svg width="24px" height="24px" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg" className="inline-block ml-2">
+                  <path d="M9.71069 18.2929C10.1012 18.6834 10.7344 18.6834 11.1249 18.2929L16.0123 13.4006C16.7927 12.6195 16.7924 11.3537 16.0117 10.5729L11.1213 5.68254C10.7308 5.29202 10.0976 5.29202 9.70708 5.68254C9.31655 6.07307 9.31655 6.70623 9.70708 7.09676L13.8927 11.2824C14.2833 11.6729 14.2833 12.3061 13.8927 12.6966L9.71069 16.8787C9.32016 17.2692 9.32016 17.9023 9.71069 18.2929Z" fill="#FFFFFF"/>
+                </svg>
+              </p>
+            </a>
+          </div>
+          <div className="text-center flex flex-col gap-8 py-8 items-center w-full">
+            <div className="text-center flex flex-col gap-8 py-16 items-center w-full max-w-5xl">
+              <p className="text-[48px] mt-4 px-6 text-black">
+                Simplicity and Flexibility at its core.
+              </p>
+              <p className="text-[24px] px-6 text-black">
+                CoMedAI is designed to be simple and easy to use, yet powerful and flexible.
+              </p>
+            </div>
+            <div className="text-center flex flex-row gap-8 py-16 items-center flex-wrap">
+              <Image
+                src={addObservation}
+                width={800}
+                height={800}
+                alt="Add Observation"
+                className="img-shadow rounded-[35px] w-[250px]"
+                title="Add Observation"
+              />
+              <Image
+                src={saveConclusion}
+                width={800}
+                height={800}
+                alt="Save Conclusion"
+                className="img-shadow rounded-[35px] w-[250px]"
+                title="Save Conclusion"
+                />
+              <Image
+                src={manageObservation}
+                width={800}
+                height={800}
+                alt="Manage Observation"
+                className="img-shadow rounded-[35px] w-[250px]"
+                title="Manage Observation"
+              />
+              <Image
+                src={generateReport}
+                width={800}
+                height={800}
+                alt="Generate Report"
+                className="img-shadow rounded-[35px] w-[250px]"
+                title="Generate Report"
+              />
+            </div>
           </div>
         </div>
       </main>
-    <Footer/>
-    </div> 
-  ); 
+      <Footer />
+    </div>
+  );
 }
