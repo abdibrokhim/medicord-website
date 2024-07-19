a6ec153fe76696ad89331477b73ae7f6ed1317e2
abdibrokhim <abdibrokhim@gmail.com>
2024-07-20 01:35:40 +0500

    update

--- src/app/firebase.config.tsx
+++ src/app/firebase.config.tsx
@@ -7,13 +7,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
+  apiKey: "AIzaSyAtHvwLZ0Xnf5yKBy4eHxqdzdL2Khxh5_8",
+  authDomain: "comed-27032024.firebaseapp.com",
+  projectId: "comed-27032024",
+  storageBucket: "comed-27032024.appspot.com",
+  messagingSenderId: "363441921131",
+  appId: "1:363441921131:web:fccdc907114ba51df2a52e",
+  measurementId: "G-806NTYPRES",
 };
 
 // Initialize Firebase
